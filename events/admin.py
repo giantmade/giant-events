e25728a3c931ffe4c880ea7630c269d38c66bf13
Will-Hoey <will.hoey@hotmail.co.uk>
2020-07-08 10:11:14 +0100

    Refactor admin to use get_ methods

--- events/admin.py
+++ events/admin.py
@@ -22,47 +22,60 @@ class EventAdmin(admin.ModelAdmin):
     Admin config for Event model
     """
 
-    list_display = getattr(
-        settings,
-        "EVENT_ADMIN_LIST_DISPLAY",
-        ["title", "start_at", "end_at", "is_published"],
-    )
-
-    fieldsets = getattr(
-        settings,
-        "EVENT_ADMIN_FIELDSETS",
-        (
-            (
-                "Details",
-                {
-                    "fields": [
-                        "title",
-                        "slug",
-                        "intro",
-                        "start_at",
-                        "end_at",
-                        "address",
-                        "location",
-                        "tags",
-                    ]
-                },
-            ),
-            ("Image", {"fields": ["photo"]}),
-            ("Publishing", {"fields": ["is_published", "publish_at"]}),
-            ("Metadata", {"classes": ("collapse",), "fields": ["created_at", "updated_at"]},),
-        ),
-    )
-    search_fields = getattr(
-        settings, "EVENT_ADMIN_SEARCH_FIELDS", ["title", "location"]
-    )
-    readonly_fields = getattr(
-        settings, "EVENT_ADMIN_READONLY_FIELDS", ["created_at", "updated_at"]
-    )
-    list_filter = getattr(
-        settings, "EVENT_ADMIN_FILTER_FIELDS", ["is_published"]
-    )
-
+    list_display = ["title", "start_at", "end_at", "is_published"]
+    search_fields = ["title", "location"]
+    readonly_fields = ["created_at", "updated_at"]
+    list_filter = ["is_published"]
     prepopulated_fields = {"slug": ["title"]}
+
+    fieldsets = [
+        (
+            "Details",
+            {
+                "fields": [
+                    "title",
+                    "slug",
+                    "intro",
+                    "start_at",
+                    "end_at",
+                    "address",
+                    "location",
+                    "tags",
+                ]
+            },
+        ),
+        ("Image", {"fields": ["photo"]}),
+        ("Publishing", {"fields": ["is_published", "publish_at"]}),
+        (
+            "Metadata",
+            {"classes": ("collapse",), "fields": ["created_at", "updated_at"]},
+        ),
+    ]
+
+    def get_list_display(self, request):
+        if hasattr(settings, "EVENT_ADMIN_LIST_DISPLAY"):
+            return settings.EVENT_ADMIN_LIST_DISPLAY
+        return super().get_list_display(request)
+
+    def get_fieldsets(self, request, obj=None):
+        if hasattr(settings, "EVENT_ADMIN_FIELDSETS"):
+            return settings.EVENT_ADMIN_FIELDSETS
+        return super().get_fieldsets(request, obj)
+
+    def get_search_fields(self, request):
+        if hasattr(settings, "EVENT_ADMIN_SEARCH_FIELDS"):
+            return settings.EVENT_ADMIN_SEARCH_FIELDS
+        return super().get_search_fields(request)
+
+    def get_readonly_fields(self, request, obj=None):
+        if hasattr(settings, "EVENT_ADMIN_READONLY_FIELDS"):
+            return settings.EVENT_ADMIN_READONLY_FIELDS
+        return super().get_readonly_fields(request, obj)
+
+    def get_list_filter(self, request):
+        if hasattr(settings, "EVENT_ADMIN_FILTER_FIELDS"):
+            return settings.EVENT_ADMIN_FILTER_FIELDS
+        return super().get_list_filter(request)
 
 
 @admin.register(Location)
